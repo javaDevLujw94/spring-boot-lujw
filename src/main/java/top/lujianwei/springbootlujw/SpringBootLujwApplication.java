71d1702d3f546e43365303148679191c991e56a6
ljw1264654273 <1264654273@qq.com>
2017-12-01 14:35:48 +0800

    jenkins

---
+++ src/main/java/top/lujianwei/springbootlujw/SpringBootLujwApplication.java
@@ -0,0 +1,12 @@
+package top.lujianwei.springbootlujw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLujwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLujwApplication.class, args);
+	}
+}
